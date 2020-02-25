1b737c96418ac5fe4ea531a0afbcb61c3f8cbff8
Your Name <you@example.com>
2020-02-26 01:11:39 +0200

    added volume calculation

--- AreaApp/src/main/java/za/ac/cput/App.java
+++ AreaApp/src/main/java/za/ac/cput/App.java
@@ -14,18 +14,24 @@ public class App
         final double PI = 3.142159;
 
         //declaring variables
-        double radius, area;  // declaring two variables
+        double radius, area, height, volume;  // declaring two variables
 
         //input
         Scanner sc = new Scanner(System.in);
         System.out.print("Enter the radius please: ");
         radius = sc.nextDouble();
 
-
         //processing
         area = PI * radius * radius;
 
         //output
         System.out.printf("The area is %f\n", area);
+
+        System.out.print("To calculate the volume enter the height of a cylinder: ");
+        height = sc.nextDouble();
+        volume = area * height;
+        System.out.println("The volume of the cylinder is: " + volume);
+
+
     }
 }
